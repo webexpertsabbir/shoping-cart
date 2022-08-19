22d6997a13a38ba251877c253db56b465d487d6f
Sabbir <webexpertsabbir@gmail.com>
2022-08-19 10:32:28 +0600

    new price

--- js/case.js
+++ js/case.js
@@ -1,8 +1,37 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberFieldStiring = caseNumberField.value;
-    const previousCaseNUmber = parseFloat(caseNumberFieldStiring);
+    const previousCaseNUmber = parseInt(caseNumberFieldStiring);
 
-    const newCaseNumber = previousCaseNUmber + 1;
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNUmber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNUmber -1;
+    }
     caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+    const newTotalPrice = newCaseNumber * 59;
+    const caseTotalPrice = document.getElementById('total-price');
+    caseTotalPrice.innerText = newTotalPrice;
+})
+
+document.getElementById('btn-case-minas').addEventListener('click', function(){
+    // const caseNumberField = document.getElementById('case-number-field');
+    // const caseNumberFieldStiring = caseNumberField.value;
+    // const previousCaseNUmber = parseFloat(caseNumberFieldStiring);
+    // if(previousCaseNUmber > 0){
+    //     const newCaseNumber = previousCaseNUmber - 1;
+    // caseNumberField.value = newCaseNumber;
+
+    // }
+    updateCaseNumber(false);
 })
